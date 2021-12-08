5e6eaa99e071e8471446ca269e945c3a63465f30
jizai <1414589221@qq.com>
2021-12-08 14:14:00 +0800

    examples

--- examples/components/v-sign/v-sign-pen.vue
+++ examples/components/v-sign/v-sign-pen.vue
@@ -1,41 +1,49 @@
 <template>
 	<view class="v-sign-pen">
-		<view class="label">
-			<view class="title">画笔大小：</view>
-			<view class="current">
-				<view
-					class="circle"
-					:style="[
-						{
-							width: currentSelect.csize + 'px',
-							height: currentSelect.csize + 'px',
-							background: color
-						},
-						customStyle
-					]"
-				></view>
-			</view>
-		</view>
+		<view class="label">{{ label }}</view>
 		<view class="options">
 			<view
-				class="opt-item"
+				:class="['opt-item', 'type-' + type]"
+				:style="[{ marginRight: space + 'rpx' }]"
 				v-for="item in csizes"
 				:key="item.size"
 				@click="onItemClick(item)"
 			>
-				<view
-					class="circle"
-					:style="[
-						{
-							width: item.csize + 'px',
-							height: item.csize + 'px',
-							background: currentSelect.size === item.size ? activeColor : color,
-							boxShadow:
-								currentSelect.size === item.size ? `0 0 2px 0 ${activeColor}` : ''
-						},
-						customStyle
-					]"
-				></view>
+				<!-- 圆点 -->
+				<template v-if="type === type_style.CIRCLE">
+					<view
+						class="circle"
+						:style="[
+							{
+								width: item.csize + 'px',
+								height: item.csize + 'px',
+								background: currentSelect.size === item.size ? activeColor : color,
+								boxShadow:
+									border && currentSelect.size === item.size
+										? `0 0 0 1px ${activeColor}`
+										: ''
+							},
+							customStyle
+						]"
+					></view>
+				</template>
+				<!-- 线 -->
+				<template v-else-if="type === type_style.LINE">
+					<view
+						class="line"
+						:style="[
+							{
+								height: item.size + 'px',
+								background: currentSelect.size === item.size ? activeColor : color,
+								boxShadow:
+									border && currentSelect.size === item.size
+										? `0 0 0 1px ${activeColor}`
+										: '',
+								customStyle
+							}
+						]"
+					></view>
+				</template>
 			</view>
 		</view>
 	</view>
@@ -46,10 +54,13 @@
  * v-sign-pen 画笔（v-sign 子组件）
  * @description 控制 v-sign 画笔的线宽
  * @tutorial
- * @property {Array} sizes 画笔大小数组，单位是px
- * @property {String} color 圆点颜色
+ * @property {String} label 标签
+ * @property {Array}  sizes 画笔大小数组，单位是px
+ * @property {String} color 选项颜色
  * @property {String} activeColor 选中项颜色
- * @property {Object} customStyle 圆点自定义样式
+ * @property {Boolean} border 选中项是否有边框
+ * @property {String} space 选项间隙
+ * @property {Object} customStyle 选项自定义样式
  * @property {Number} bigger 圆点变大变粗倍数
  * @property {Number} minSize 圆点最小大小，单位 px
  * @event {Function} change 选择画笔大小时触发
@@ -57,15 +68,28 @@
  **/
 // v-sign 父组件提供的接口
 let vSignInterface
+// 选项样式
+const type_style = {
+	CIRCLE: 'circle',
+	LINE: 'line'
+}
 export default {
 	name: 'v-sign-pen',
 	props: {
+		// 选项样式
+		type: {
+			type: String,
+			default: type_style.CIRCLE
+		},
+		label: {
+			type: String,
+		},
 		// 画笔大小数组，单位是px
 		sizes: {
 			type: Array,
 			default: () => [2, 4, 6, 8, 10]
 		},
-		// 圆点颜色
+		// 选项颜色
 		color: {
 			type: String,
 			default: '#333'
@@ -75,7 +99,17 @@ export default {
 			type: String,
 			default: '#333'
 		},
-		// 圆点自定义样式
+		// 选中项是否有边框
+		border: {
+			type: Boolean,
+			default: true
+		},
+		// 选项间隙
+		space: {
+			type: Number,
+			default: 20
+		},
+		// 选项自定义样式
 		customStyle: {
 			type: Object,
 			default: () => ({})
@@ -94,6 +128,7 @@ export default {
 	inject: ['getInterface'],
 	data() {
 		return {
+			type_style,
 			currentSelect: null,
 			csizes: []
 		}
@@ -127,28 +162,35 @@ export default {
 
 <style lang="scss" scoped>
 .v-sign-pen {
+	padding: 12rpx;
+	background: #fff;
 	.label {
-		display: flex;
-		align-items: center;
-		min-height: 40rpx;
-		.title {
-			font-sizes: 28rpx;
-			color: #666;
-		}
-		.current {
-		}
+		font-size: 28rpx;
+		color: #333;
 	}
 	.circle {
 		border-radius: 50%;
 		margin: 0 auto;
 		border: 4rpx solid #fff;
 	}
+	.line {
+		width: 20px;
+		margin: 0 auto;
+		border: 4rpx solid #fff;
+	}
 	.options {
 		display: flex;
-		margin: 20rpx;
+		align-items: flex-end;
 		.opt-item {
-			min-width: 40rpx;
-			margin-right: 20rpx;
+			height: 100%;
+			&.type-circle {
+				min-width: 40rpx;
+			}
+			&.type-line {
+				min-height: 20px;
+				display: flex;
+				align-items: flex-end;
+			}
 			&:last-child {
 				margin-right: 0;
 			}
--- examples/pages/v-sign-test/index.vue
+++ examples/pages/v-sign-test/index.vue
@@ -1,15 +1,22 @@
 <template>
 	<view class="g-container">
-		<cus-card title="v-sign">
-			<v-sign height="600" :customStyle="{ background: 'orange' }">
+		<!-- <cus-card title="v-sign"> -->
+		<v-sign :customStyle="{ background: 'orange' }">
+			<view class="control-area">
+				<view>按钮组件：</view>
 				<v-sign-action
 					:border="false"
 					:customStyle="{ 'margin-top': '20rpx' }"
 					@save="save"
 				></v-sign-action>
-				<v-sign-pen></v-sign-pen>
-			</v-sign>
-		</cus-card>
+				<view>画笔组件：</view>
+				<v-sign-pen label="默认样式"></v-sign-pen>
+				<v-sign-pen label="样式2" active-color="orange"></v-sign-pen>
+				<v-sign-pen label="样式3" type="line"></v-sign-pen>
+				<v-sign-pen label="样式4" type="line" :border="false" active-color="orange"></v-sign-pen>
+			</view>
+		</v-sign>
+		<!-- </cus-card> -->
 	</view>
 </template>
 
@@ -32,8 +39,14 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
 .g-container {
-	padding: 10px 20px;
+	/* padding: 10px 20px; */
+	width: 100vh;
+	height: 100vh;
+	.control-area {
+		// position: absolute;
+		bottom: 0;
+	}
 }
 </style>
